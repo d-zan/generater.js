aa4a026de4eda737a54a3e10b831757c27c947f5
Yousef Karm <143213837+d-zan@users.noreply.github.com>
2024-12-01 22:48:04 +0200

    Update colors.js
    Remove Comments

--- types/colors.js
+++ types/colors.js
@@ -1,306 +1,302 @@
 const calmHEX = [
-  "#FF5733", // Red Orange
-  "#33FF57", // Green
-  "#3357FF", // Blue
-  "#F1C40F", // Bright Yellow
-  "#8E44AD", // Purple
-  "#E74C3C", // Tomato Red
-  "#3498DB", // Sky Blue
-  "#2ECC71", // Emerald Green
-  "#F39C12", // Orange
-  "#D35400", // Pumpkin
-  "#1ABC9C", // Turquoise
-  "#9B59B6", // Amethyst
-  "#34495E", // Dark Blue Gray
-  "#E67E22", // Carrot
-  "#16A085", // Dark Turquoise
-  "#C0392B", // Red
-  "#2980B9", // Ocean Blue
-  "#F4D03F", // Sunflower Yellow
-  "#7D3C98", // Purple Plum
-  "#B2B2B2", // Light Gray
-  "#FF6F61", // Coral
-  "#6A5ACD", // Slate Blue
-  "#FFD700", // Gold
-  "#FF1493", // Deep Pink
-  "#20B2AA", // Light Sea Green
-  "#FF4500", // Orange Red
-  "#DA70D6", // Orchid
-  "#ADFF2F", // Green Yellow
-  "#7FFF00", // Chartreuse
-  "#D2691E", // Chocolate
-  "#FFB6C1", // Light Pink
-  "#4682B4", // Steel Blue
-  "#FF8C00", // Dark Orange
-  "#8B0000", // Dark Red
-  "#3CB371", // Medium Sea Green
-  "#FFDAB9", // Peach Puff
-  "#8B008B", // Dark Magenta
-  "#B22222", // Firebrick
-  "#FF6347", // Tomato
-  "#C0C0C0"  // Silver
+  "#FF5733", 
+  "#33FF57", 
+  "#3357FF", 
+  "#F1C40F", 
+  "#8E44AD", 
+  "#E74C3C", 
+  "#3498DB", 
+  "#2ECC71", 
+  "#F39C12", 
+  "#D35400", 
+  "#1ABC9C", 
+  "#9B59B6", 
+  "#34495E", 
+  "#E67E22", 
+  "#16A085", 
+  "#C0392B", 
+  "#2980B9", 
+  "#F4D03F", 
+  "#7D3C98", 
+  "#B2B2B2", 
+  "#FF6F61", 
+  "#6A5ACD", 
+  "#FFD700", 
+  "#FF1493", 
+  "#20B2AA", 
+  "#FF4500", 
+  "#DA70D6", 
+  "#ADFF2F", 
+  "#7FFF00", 
+  "#D2691E", 
+  "#FFB6C1", 
+  "#4682B4", 
+  "#FF8C00", 
+  "#8B0000", 
+  "#3CB371", 
+  "#FFDAB9", 
+  "#8B008B", 
+  "#B22222", 
+  "#FF6347", 
+  "#C0C0C0"  
 ];
 const calmRGB = [
-  "rgb(255, 87, 51)",   // Red Orange
-  "rgb(51, 255, 87)",   // Green
-  "rgb(51, 87, 255)",   // Blue
-  "rgb(241, 196, 15)",  // Bright Yellow
-  "rgb(142, 68, 173)",  // Purple
-  "rgb(231, 76, 60)",   // Tomato Red
-  "rgb(52, 152, 219)",  // Sky Blue
-  "rgb(46, 204, 113)",  // Emerald Green
-  "rgb(243, 156, 18)",  // Orange
-  "rgb(211, 84, 0)",    // Pumpkin
-  "rgb(26, 188, 156)",  // Turquoise
-  "rgb(155, 89, 182)",  // Amethyst
-  "rgb(52, 73, 94)",    // Dark Blue Gray
-  "rgb(230, 126, 34)",  // Carrot
-  "rgb(22, 160, 133)",  // Dark Turquoise
-  "rgb(192, 57, 43)",   // Red
-  "rgb(41, 128, 185)",  // Ocean Blue
-  "rgb(244, 208, 63)",  // Sunflower Yellow
-  "rgb(125, 60, 152)",  // Purple Plum
-  "rgb(178, 178, 178)",  // Light Gray
-  "rgb(255, 111, 97)",  // Coral
-  "rgb(106, 90, 205)",  // Slate Blue
-  "rgb(255, 215, 0)",   // Gold
-  "rgb(255, 20, 147)",  // Deep Pink
-  "rgb(32, 178, 170)",  // Light Sea Green
-  "rgb(255, 69, 0)",    // Orange Red
-  "rgb(218, 112, 214)", // Orchid
-  "rgb(173, 255, 47)",  // Green Yellow
-  "rgb(127, 255, 0)",   // Chartreuse
-  "rgb(210, 105, 30)",  // Chocolate
-  "rgb(255, 182, 193)", // Light Pink
-  "rgb(70, 130, 180)",  // Steel Blue
-  "rgb(255, 140, 0)",   // Dark Orange
-  "rgb(139, 0, 0)",     // Dark Red
-  "rgb(60, 179, 113)",  // Medium Sea Green
-  "rgb(255, 218, 185)", // Peach Puff
-  "rgb(139, 0, 139)",   // Dark Magenta
-  "rgb(178, 34, 34)",   // Firebrick
-  "rgb(255, 99, 71)",   // Tomato
-  "rgb(192, 192, 192)"  // Silver
-];
-///
-const blueHEX = [
-  "#0000FF",      // Blue
-  "#0000CC",      // Medium Blue
-  "#000099",      // Dark Blue
-  "#000066",      // Very Dark Blue
-  "#000080",      // Navy
-  "#0066CC",      // Sky Blue
-  "#00BFFF",      // Vivid Blue
-  "#00CED1",      // Deep Sky Blue
-  "#1E90FF",      // Dodger Blue
-  "#4682B4",      // Steel Blue
-  "#6495ED",      // Cornflower Blue
-  "#87CEFA",      // Light Sky Blue
-  "#87CEEB",      // Sky Blue
-  "#ADD8E6",      // Light Blue
-  "#B0E0E6",      // Powder Blue
-  "#F0F8FF",      // Alice Blue
-  "#B0E0E6",      // Light Steel Blue
-  "#00CED1",      // Dark Turquoise
-  "#4682B4",      // Steel Blue
-  "#1E90FF",      // Dodger Blue
-  "#00BFFF",      // Deep Sky Blue
-  "#000080",      // Navy
-  "#0000CC",      // Medium Blue
-  "#000099",      // Dark Blue
-  "#191970",      // Midnight Blue
-  "#4169E1",      // Royal Blue
-  "#87CEFA",      // Light Sky Blue
-  "#4682B4",      // Steel Blue
-  "#0000FF",      // Blue
-  "#0066CC",      // Medium Blue
-  "#00BFFF",      // Vivid Blue
-  "#00CED1",      // Deep Sky Blue
-  "#00CCCC",      // Light Sea Green
-  "#0000FF",      // Blue
-  "#6495ED",      // Cornflower Blue
-  "#87CEFA",      // Light Sky Blue
-  "#ADD8E6",      // Light Blue
-  "#F0F8FF",      // Alice Blue
-  "#B0E0E6",      // Light Steel Blue
-  "#4682B4",      // Steel Blue
-  "#00CED1",      // Dark Turquoise
-  "#1E90FF",      // Dodger Blue
-  "#00BFFF"       // Deep Sky Blue
-];
-const blueRGB = [
-  "rgb(0, 0, 255)",      // Blue
-  "rgb(0, 0, 204)",      // Medium Blue
-  "rgb(0, 0, 153)",      // Dark Blue
-  "rgb(0, 0, 102)",      // Very Dark Blue
-  "rgb(0, 0, 128)",      // Navy
-  "rgb(0, 102, 204)",    // Sky Blue
-  "rgb(0, 128, 255)",    // Vivid Blue
-  "rgb(0, 191, 255)",    // Deep Sky Blue
-  "rgb(30, 144, 255)",   // Dodger Blue
-  "rgb(70, 130, 180)",   // Steel Blue
-  "rgb(100, 149, 237)",  // Cornflower Blue
-  "rgb(135, 206, 250)",  // Light Sky Blue
-  "rgb(135, 206, 235)",  // Sky Blue
-  "rgb(173, 216, 230)",  // Light Blue
-  "rgb(176, 224, 230)",  // Powder Blue
-  "rgb(240, 248, 255)",  // Alice Blue
-  "rgb(176, 224, 230)",  // Light Steel Blue
-  "rgb(0, 206, 209)",    // Dark Turquoise
-  "rgb(70, 130, 180)",   // Steel Blue
-  "rgb(30, 144, 255)",   // Dodger Blue
-  "rgb(0, 191, 255)",    // Deep Sky Blue
-  "rgb(0, 0, 128)",      // Navy
-  "rgb(0, 0, 205)",      // Medium Blue
-  "rgb(0, 0, 139)",      // Dark Blue
-  "rgb(25, 25, 112)",    // Midnight Blue
-  "rgb(65, 105, 225)",   // Royal Blue
-  "rgb(135, 206, 235)",  // Light Sky Blue
-  "rgb(70, 130, 180)",   // Steel Blue
-  "rgb(0, 0, 255)",      // Blue
-  "rgb(0, 102, 204)",    // Medium Blue
-  "rgb(0, 128, 255)",    // Vivid Blue
-  "rgb(0, 191, 255)",    // Deep Sky Blue
-  "rgb(0, 204, 204)",    // Light Sea Green
-  "rgb(0, 0, 255)",      // Blue
-  "rgb(100, 149, 237)",  // Cornflower Blue
-  "rgb(135, 206, 250)",  // Light Sky Blue
-  "rgb(173, 216, 230)",  // Light Blue
-  "rgb(240, 248, 255)",  // Alice Blue
-  "rgb(176, 224, 230)",  // Light Steel Blue
-  "rgb(70, 130, 180)",   // Steel Blue
-  "rgb(0, 206, 209)",    // Dark Turquoise
-  "rgb(30, 144, 255)",   // Dodger Blue
-  "rgb(0, 191, 255)"     // Deep Sky Blue
-];
-const darkHEX = [
-  "#000000", // Black
-  "#1C1C1C", // Dark Gray
-  "#2F4F4F", // Dark Slate Gray
-  "#4B0082", // Indigo
-  "#4A4A4A", // Dim Gray
-  "#696969", // Dim Gray
-  "#708090", // Slate Gray
-  "#2E2E2E", // Dark Charcoal
-  "#3B3B3B", // Dark Gray
-  "#4B0082"  // Indigo
+  "rgb(255, 87, 51)",   
+  "rgb(51, 255, 87)",   
+  "rgb(51, 87, 255)",   
+  "rgb(241, 196, 15)",  
+  "rgb(142, 68, 173)",  
+  "rgb(231, 76, 60)",   
+  "rgb(52, 152, 219)",  
+  "rgb(46, 204, 113)",  
+  "rgb(243, 156, 18)",  
+  "rgb(211, 84, 0)",    
+  "rgb(26, 188, 156)",  
+  "rgb(155, 89, 182)",  
+  "rgb(52, 73, 94)",    
+  "rgb(230, 126, 34)",  
+  "rgb(22, 160, 133)",  
+  "rgb(192, 57, 43)",   
+  "rgb(41, 128, 185)",  
+  "rgb(244, 208, 63)",  
+  "rgb(125, 60, 152)",  
+  "rgb(178, 178, 178)",  
+  "rgb(255, 111, 97)",  
+  "rgb(106, 90, 205)",  
+  "rgb(255, 215, 0)",   
+  "rgb(255, 20, 147)",  
+  "rgb(32, 178, 170)",  
+  "rgb(255, 69, 0)",    
+  "rgb(218, 112, 214)", 
+  "rgb(173, 255, 47)",  
+  "rgb(127, 255, 0)",   
+  "rgb(210, 105, 30)",  
+  "rgb(255, 182, 193)", 
+  "rgb(70, 130, 180)",  
+  "rgb(255, 140, 0)",   
+  "rgb(139, 0, 0)",     
+  "rgb(60, 179, 113)",  
+  "rgb(255, 218, 185)", 
+  "rgb(139, 0, 139)",   
+  "rgb(178, 34, 34)",   
+  "rgb(255, 99, 71)",   
+  "rgb(192, 192, 192)"  
 ];
 
+const blueHEX = [
+  "#0000FF",      
+  "#0000CC",      
+  "#000099",      
+  "#000066",      
+  "#000080",      
+  "#0066CC",      
+  "#00BFFF",      
+  "#00CED1",      
+  "#1E90FF",      
+  "#4682B4",      
+  "#6495ED",      
+  "#87CEFA",      
+  "#87CEEB",      
+  "#ADD8E6",      
+  "#B0E0E6",      
+  "#F0F8FF",      
+  "#B0E0E6",      
+  "#00CED1",      
+  "#4682B4",      
+  "#1E90FF",      
+  "#00BFFF",      
+  "#000080",      
+  "#0000CC",      
+  "#000099",      
+  "#191970",      
+  "#4169E1",      
+  "#87CEFA",      
+  "#4682B4",      
+  "#0000FF",      
+  "#0066CC",      
+  "#00BFFF",      
+  "#00CED1",      
+  "#00CCCC",      
+  "#0000FF",      
+  "#6495ED",      
+  "#87CEFA",      
+  "#ADD8E6",      
+  "#F0F8FF",      
+  "#B0E0E6",      
+  "#4682B4",      
+  "#00CED1",      
+  "#1E90FF",      
+  "#00BFFF"       
+];
+const blueRGB = [
+  "rgb(0, 0, 255)",      
+  "rgb(0, 0, 204)",      
+  "rgb(0, 0, 153)",      
+  "rgb(0, 0, 102)",      
+  "rgb(0, 0, 128)",      
+  "rgb(0, 102, 204)",    
+  "rgb(0, 128, 255)",    
+  "rgb(0, 191, 255)",    
+  "rgb(30, 144, 255)",   
+  "rgb(70, 130, 180)",   
+  "rgb(100, 149, 237)",  
+  "rgb(135, 206, 250)",  
+  "rgb(135, 206, 235)",  
+  "rgb(173, 216, 230)",  
+  "rgb(176, 224, 230)",  
+  "rgb(240, 248, 255)",  
+  "rgb(176, 224, 230)",  
+  "rgb(0, 206, 209)",    
+  "rgb(70, 130, 180)",   
+  "rgb(30, 144, 255)",   
+  "rgb(0, 191, 255)",    
+  "rgb(0, 0, 128)",      
+  "rgb(0, 0, 205)",      
+  "rgb(0, 0, 139)",      
+  "rgb(25, 25, 112)",    
+  "rgb(65, 105, 225)",   
+  "rgb(135, 206, 235)",  
+  "rgb(70, 130, 180)",   
+  "rgb(0, 0, 255)",      
+  "rgb(0, 102, 204)",    
+  "rgb(0, 128, 255)",    
+  "rgb(0, 191, 255)",    
+  "rgb(0, 204, 204)",    
+  "rgb(0, 0, 255)",      
+  "rgb(100, 149, 237)",  
+  "rgb(135, 206, 250)",  
+  "rgb(173, 216, 230)",  
+  "rgb(240, 248, 255)",  
+  "rgb(176, 224, 230)",  
+  "rgb(70, 130, 180)",   
+  "rgb(0, 206, 209)",    
+  "rgb(30, 144, 255)",   
+  "rgb(0, 191, 255)"     
+];
+
+const darkHEX = [
+  "#000000", 
+  "#1C1C1C", 
+  "#2F4F4F", 
+  "#4B0082", 
+  "#4A4A4A", 
+  "#696969", 
+  "#708090", 
+  "#2E2E2E", 
+  "#3B3B3B", 
+  "#4B0082"  
+];
 const darkRGB = [
-  "rgb(0, 0, 0)",        // Black
-  "rgb(28, 28, 28)",     // Dark Gray
-  "rgb(47, 79, 79)",     // Dark Slate Gray
-  "rgb(75, 0, 130)",     // Indigo
-  "rgb(74, 74, 74)",     // Dim Gray
-  "rgb(105, 105, 105)",  // Dim Gray
-  "rgb(112, 128, 144)",  // Slate Gray
-  "rgb(46, 46, 46)",     // Dark Charcoal
-  "rgb(59, 59, 59)",     // Dark Gray
-  "rgb(75, 0, 130)"      // Indigo
+  "rgb(0, 0, 0)",        
+  "rgb(28, 28, 28)",     
+  "rgb(47, 79, 79)",     
+  "rgb(75, 0, 130)",     
+  "rgb(74, 74, 74)",     
+  "rgb(105, 105, 105)",  
+  "rgb(112, 128, 144)",  
+  "rgb(46, 46, 46)",     
+  "rgb(59, 59, 59)",     
+  "rgb(75, 0, 130)"      
 ];
 
 const greenHEX = [
-  "#008000", // Green
-  "#006400", // Dark Green
-  "#00FF00", // Lime
-  "#32CD32", // Lime Green
-  "#3CB371", // Medium Sea Green
-  "#2E8B57", // Sea Green
-  "#228B22", // Forest Green
-  "#ADFF2F", // Green Yellow
-  "#7FFF00", // Chartreuse
-  "#9ACD32"  // Yellow Green
+  "#008000", 
+  "#006400", 
+  "#00FF00", 
+  "#32CD32", 
+  "#3CB371", 
+  "#2E8B57", 
+  "#228B22", 
+  "#ADFF2F", 
+  "#7FFF00", 
+  "#9ACD32"  
 ];
-
 const greenRGB = [
-  "rgb(0, 128, 0)",        // Green
-  "rgb(0, 100, 0)",        // Dark Green
-  "rgb(0, 255, 0)",        // Lime
-  "rgb(50, 205, 50)",      // Lime Green
-  "rgb(60, 179, 113)",     // Medium Sea Green
-  "rgb(46, 139, 87)",      // Sea Green
-  "rgb(34, 139, 34)",      // Forest Green
-  "rgb(173, 255, 47)",     // Green Yellow
-  "rgb(127, 255, 0)",      // Chartreuse
-  "rgb(154, 205, 50)"      // Yellow Green
+  "rgb(0, 128, 0)",        
+  "rgb(0, 100, 0)",        
+  "rgb(0, 255, 0)",        
+  "rgb(50, 205, 50)",      
+  "rgb(60, 179, 113)",     
+  "rgb(46, 139, 87)",      
+  "rgb(34, 139, 34)",      
+  "rgb(173, 255, 47)",     
+  "rgb(127, 255, 0)",      
+  "rgb(154, 205, 50)"      
 ];
 
 const randomHEX = [
-  "#FF5733", // Red-Orange
-  "#33FF57", // Green
-  "#3357FF", // Blue
-  "#F1C40F", // Bright Yellow
-  "#9B59B6", // Purple
-  "#E74C3C", // Tomato Red
-  "#3498DB", // Sky Blue
-  "#2ECC71", // Emerald Green
-  "#E67E22", // Orange
-  "#D35400"  // Pumpkin
+  "#FF5733", 
+  "#33FF57", 
+  "#3357FF", 
+  "#F1C40F", 
+  "#9B59B6", 
+  "#E74C3C", 
+  "#3498DB", 
+  "#2ECC71", 
+  "#E67E22", 
+  "#D35400"  
 ];
-
 const randomRGB = [
-  "rgb(255, 87, 51)",   // Red-Orange
-  "rgb(51, 255, 87)",   // Green
-  "rgb(51, 87, 255)",   // Blue
-  "rgb(241, 196, 15)",  // Bright Yellow
-  "rgb(155, 89, 182)",  // Purple
-  "rgb(231, 76, 60)",   // Tomato Red
-  "rgb(52, 152, 219)",  // Sky Blue
-  "rgb(46, 204, 113)",  // Emerald Green
-  "rgb(243, 156, 18)",  // Orange
-  "rgb(211, 84, 0)"     // Pumpkin
+  "rgb(255, 87, 51)",   
+  "rgb(51, 255, 87)",   
+  "rgb(51, 87, 255)",   
+  "rgb(241, 196, 15)",  
+  "rgb(155, 89, 182)",  
+  "rgb(231, 76, 60)",   
+  "rgb(52, 152, 219)",  
+  "rgb(46, 204, 113)",  
+  "rgb(243, 156, 18)",  
+  "rgb(211, 84, 0)"     
 ];
 
 const redHEX = [
-  "#FF0000", // Red
-  "#B22222", // Firebrick
-  "#DC143C", // Crimson
-  "#FF6347", // Tomato
-  "#FF4500", // Orange Red
-  "#CD5C5C", // Indian Red
-  "#F08080", // Light Coral
-  "#E9967A", // Dark Salmon
-  "#FF7F50", // Coral
-  "#FA8072"   // Salmon
+  "#FF0000", 
+  "#B22222", 
+  "#DC143C", 
+  "#FF6347", 
+  "#FF4500", 
+  "#CD5C5C", 
+  "#F08080", 
+  "#E9967A", 
+  "#FF7F50", 
+  "#FA8072"   
 ];
-
 const redRGB = [
-  "rgb(255, 0, 0)",      // Red
-  "rgb (178, 34, 34)",      // Firebrick
-  "rgb(220, 20, 60)",    // Crimson
-  "rgb(255, 99, 71)",    // Tomato
-  "rgb(255, 69, 0)",     // Orange Red
-  "rgb(205, 92, 92)",    // Indian Red
-  "rgb(240, 128, 128)",  // Light Coral
-  "rgb(233, 150, 122)",  // Dark Salmon
-  "rgb(255, 127, 80)",   // Coral
-  "rgb(250, 128, 114)"   // Salmon
+  "rgb(255, 0, 0)",      
+  "rgb (178, 34, 34)",      
+  "rgb(220, 20, 60)",    
+  "rgb(255, 99, 71)",    
+  "rgb(255, 69, 0)",     
+  "rgb(205, 92, 92)",    
+  "rgb(240, 128, 128)",  
+  "rgb(233, 150, 122)",  
+  "rgb(255, 127, 80)",   
+  "rgb(250, 128, 114)"   
 ];
 
 const whiteHEX = [
-  "#FFFFFF", // White
-  "#F8F8FF", // Ghost White
-  "#FFFAFA", // Snow
-  "#F0FFF0", // Honeydew
-  "#F5FFFA", // Mint Cream
-  "#F0FFFF", // Azure
-  "#F0F8FF", // Alice Blue
-  "#FFF0F5", // Lavender Blush
-  "#FFE4E1", // Misty Rose
-  "#FFF5EE"   // Seashell
+  "#FFFFFF", 
+  "#F8F8FF", 
+  "#FFFAFA", 
+  "#F0FFF0", 
+  "#F5FFFA", 
+  "#F0FFFF", 
+  "#F0F8FF", 
+  "#FFF0F5", 
+  "#FFE4E1", 
+  "#FFF5EE"   
 ];
-
 const whiteRGB = [
-  "rgb(255, 255, 255)",   // White
-  "rgb(248, 248, 255)",   // Ghost White
-  "rgb(255, 250, 250)",   // Snow
-  "rgb(240, 255, 240)",   // Honeydew
-  "rgb(245, 255, 250)",   // Mint Cream
-  "rgb(240, 255, 255)",   // Azure
-  "rgb(240, 248, 255)",   // Alice Blue
-  "rgb(255, 240, 245)",   // Lavender Blush
-  "rgb(255, 228, 225)",   // Misty Rose
-  "rgb(255, 245, 238)"    // Seashell
+  "rgb(255, 255, 255)",   
+  "rgb(248, 248, 255)",   
+  "rgb(255, 250, 250)",   
+  "rgb(240, 255, 240)",   
+  "rgb(245, 255, 250)",   
+  "rgb(240, 255, 255)",   
+  "rgb(240, 248, 255)",   
+  "rgb(255, 240, 245)",   
+  "rgb(255, 228, 225)",   
+  "rgb(255, 245, 238)"    
 ];
 
 module.exports = {

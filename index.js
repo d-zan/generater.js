b09a0ae1ad916dcb6945276a294cc4282117dc8e
Yousef Karm <143213837+d-zan@users.noreply.github.com>
2024-12-01 22:54:14 +0200

    Update index.js
    Remove the comments and add some comments

--- index.js
+++ index.js
@@ -1,47 +1,28 @@
-// Import Generaters
+//gen
 const generateKey = require('./Generaters/KeyGenerater');
 const generatePassword = require('./Generaters/PasswordGenerater');
 const generatePhoneNumber = require('./Generaters/PhoneNumberGenerater');
 const generateEmail = require('./Generaters/e-mailGenerater');
 const generateDomain = require('./Generaters/domainsGenerater');
 const generateID = require('./Generaters/IDGenerater');
-const randomColor = require('./Random/RandomColor');
-const randomNumber = require('./Random/randomNumber');
 const { generateOTP, verifyOTP } = require('./Generaters/OTPGenerater');
-
-// Credit Card Generators
 const mastercard = require('./Generaters/Credit Card/mastercardGenerater');
 const vise = require('./Generaters/Credit Card/viseGenerater');
+///random
+const randomColor = require('./Random/RandomColor');
+const randomNumber = require('./Random/randomNumber');
 
-// Export all modules
 module.exports = {
-    // Key Generators
     generateKey,
-    
-    // Password Generators
     generatePassword,
-    
-    // Phone Number Generators
     generatePhoneNumber,
-    
-    // Email Generators
     generateEmail,
-    
-    // Domain Generators
     generateDomain,
-    
-    // ID Generators
     generateID,
-    
-    // Random Generators
     randomColor,
     randomNumber,
-    
-    // OTP Generators
     generateOTP,
     verifyOTP,
-    
-    // Credit Card Generators
     mastercard,
     vise
 };
